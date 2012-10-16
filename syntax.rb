1e45fe62d5f5b302fc6e9b92ba93d7a30afef72f
Gernot Hoeflechner <1986gh@gmail.com>
2012-10-16 12:31:09 +0200

    little refactor, added documentation

--- syntax.rb
+++ syntax.rb
@@ -31,7 +31,7 @@ class StructureAnalysis
 
     hash = Hash.new
     tbo.each_with_index { |x, i| hash[i] = [x] }
-    hash.each do |k,v| # !!! CONJ nur am Satzanfang? cum?? ut mit Ind?
+    hash.each do |k,v| 
       (v[0] & VERBS).empty? ? v << false : v << true
       (v[0] & CONJUNCTIONS).empty? ? v << false : v << true
       (v[0] & PRED).empty? ? v << false : v << true
@@ -44,18 +44,15 @@ class StructureAnalysis
 
         when v[1] == false && v[2] == false
           structured << v[0]
-          hash.delete(k)
 
         when v[3] == true
           if structured[0].nil?
             structured << v[0]
           else structured[0].concat(v[0])
           end
-          hash.delete(k)
 
         when v[1] == true && v[2] == true
           structured << v[0]
-          hash.delete(k)
 
         when v[1] == false && v[2] == true && v[3] == false
           hash.each do |x,y|
@@ -65,15 +62,26 @@ class StructureAnalysis
             end
           end 
           structured << v[0]
-          hash.delete(k)
         end
+        hash.delete(k)
       end 
     end
     structured
   end
 end
-test = StructureAnalysis.new("Cui Iuno, si se pulcherrimam fuisse iudicavisset, in omnibus terris eum regnaturum pollicita est, Minerva ei praestantem gloriam promisit.")
-p test.structure
-# if word after match is included in Conjunction (take one of the check
-# methods in the library, take string till next sc.pos.
-# start from the inside.
+
+# DOCUMENTATION:
+# structure needs to know: Conjunctions, all verbs in a sentence, the predicate.
+# further analysis needed for AcI/NCI. infinite verb should trigger this
+# analysis.
+# some conj need different rules: 
+#   ut (ind.). 
+#   cum (Abl?), 
+#   rel.pron (anschluss? pronomen beforehand? (", in quo...")
+# while the rest is processing, show the user the structured sentence (nested)
+#
+#
+# PROBLEMS:
+#   et, -qui
+#   aufzählungs commas should NOT be a problem. they will split the sentence
+#   initially, but will get concatenated again - I hope... :)
